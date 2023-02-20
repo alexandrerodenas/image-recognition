4be26f20a4ab015d7634736b4e3ff3a425257c01
rodenas alexandre <rodenas.alexandre@gmail.com>
2023-02-20 16:57:57 +0100

    Move or open screenshots

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import { listFilesIn } from "./pre-processing/scanning/files-scanner";
-import { isImage } from "./pre-processing/filtering/is-image";
+import { listFilesAt } from "./file-utils/listing/list-files";
+import { isImage } from "./file-utils/identifying/is-image";
 import { RecognitionRunner } from "./image-recognition/runner/runner";
 import { RecognizedImage } from "./image-recognition/recognized-image";
 import * as config from "./config.json";  
+import { ScreenshotsHandler } from "./post-processing/screenshots/screenshots-handler";
 
 const directoryToScan = config.directoryToScan;
-const files = listFilesIn(directoryToScan);
+const files = listFilesAt(directoryToScan);
 
 (async () => {
     try {
         const recognizedImages: RecognizedImage[] = await RecognitionRunner.runRecognitionOn(
             files.filter(isImage)
         );
-        const screenshots = recognizedImages.filter(image => image.isScreenshot());
+        const screenshotsHandler = new ScreenshotsHandler(recognizedImages);
+        screenshotsHandler.moveIntoDedicatedDirectory();
     } catch (e) {
         console.error(e);
     }
--- src/post-processing/moving/move-file.ts
+++
@@ -1,5 +0,0 @@
-import * as fs from 'fs';
-
-export function move(source: string, destination: string): void {
-    fs.renameSync(source, destination);
-}
